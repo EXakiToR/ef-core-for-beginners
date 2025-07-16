c62746c79b834b71efb847e253e358b6016dc393
Andrei Elciscev <andrey.elciscev@gmail.com>
2025-07-16 10:41:33 +0000

    first migration, add email property (with no migration update)

--- parts/1-getting-started/ContosoPizza/Models/Order.cs
+++ parts/1-getting-started/ContosoPizza/Models/Order.cs
@@ -6,9 +6,7 @@ public class Order
     public DateTime OrderPlaced { get; set; }
     public DateTime? OrderFulfilled { get; set; }
     public int CustomerId { get; set; }
-
     public Customer Customer { get; set; } = null!;
-    
     public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
 }
 
